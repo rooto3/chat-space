dd0722a2d124c87d13e62780f404f1c34b5779f9
rooto3 <ruuto30310@gmail.com>
2020-02-28 17:39:03 +0900

    first_commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,27 @@
-$(function(){ 
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="list-items" data-message-id=${message.id}>
-         <div class="list-items__top">
-           <div class="list-items__top__name">
-             ${message.user_name}
-           </div>
-           <div class="list-items__top__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="list-items__down">
-           <p class="list-items__down__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="list-items" data-message-id=${message.id}>
-         <div class="list-items__top">
-           <div class="list-items__top__name">
-             ${message.user_name}
-           </div>
-           <div class="list-items__top__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="list-items__down">
-           <p class="list-items__down__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+$(function(){  
+  function buildHTML(message) {
+    image = (message.image) ? `<img class= "ist-items__down__image" src=${message.image} >` : "";
+
+
+    var html = `<div class="list-items" data-message-id="${message.id}"> 
+          <div class="list-items__top">
+            <div class="list-items__top__name">
+              ${message.user_name}
+            </div>
+            <div class="list-items__top__date">
+              ${message.date}
+            </div>
+          </div>
+          <div class="list-items__down">
+            <p class="list-items__down__content">
+              ${message.content}
+            </p>
+            ${image}
+          </div>
+        </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -64,4 +47,29 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.chat-main__message-list').append(insertHTML);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
